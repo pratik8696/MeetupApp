94dff1ba41f39735cfbfda0a748a7f28d20dbe6c
pratik8696 <mepratikanand@gmail.com>
2021-09-08 16:07:55 +0530

    ssr

--- pages/index.js
+++ pages/index.js
@@ -24,12 +24,33 @@ const Dummy_meetup=[
     },
 ]
 
-function Homepage() {
+function Homepage(props) {
     return (
         <div>
-            <MeetupList meetups={Dummy_meetup} />
+            <MeetupList meetups={props.meetups} />
         </div>
     )
 }
+// export async function getServerSideProps(context)
+// {
+//     const req=context.req;
+//     const res=context.res;
+//     return{
+//         props:{
+//             meetups:Dummy_meetup
+//         },
+//     }
+// }
+export async function getStaticProps()
+{
+    return {
+        props:{
+            meetups:Dummy_meetup,
+        },
+        revalidate:1
+    }
+}
+
+// so there are 2 methods staticsitegeneration and other one is serversiderendering
 
 export default Homepage;
