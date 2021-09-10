b6110238935919706b77ec53312e1dcf6aba2ab7
pratik8696 <mepratikanand@gmail.com>
2021-09-10 12:39:03 +0530

    ready to connect with database

--- pages/[meetupid]/index.js
+++ pages/[meetupid]/index.js
@@ -4,43 +4,46 @@ function meetups(props) {
     return (
         <div>
             <MeetupDetails
-                img={props.img}
-                title={props.title}
-                address={props.address}
-                description={props.description}
+                img={props.meetup.img}
+                title={props.meetup.title}
+                address={props.meetup.address}
+                description={props.meetup.description}
             />
         </div>
     )
 }
 
-export async function getStaticPaths()
-{
+export async function getStaticPaths() {
     return {
-        fallback:true,
-        paths:[
+        fallback: true,
+        paths: [
             {
-                meetupid:'1',
+                params: {
+                    meetupid: '1',
+                }
             },
             {
-                meetupid: '2',
+
+                params: {
+                    meetupid: '2',
+                }
             },
         ]
     }
 }
 
-export async function getStaticProps(context)
-{
-    const route=context.params.meetupid;
+export async function getStaticProps(context) {
+    const route = context.params.meetupid;
     console.log(route);
 
     return {
-        props:{
-            meetup:{
-                id:route,
-                img:"https://picsum.photos/3840/2160",
-                title:"A First Meetup",
-                address:"STreet no 5 block 21B",
-                description:"Deserunt aute sint laborum quis nisi deserunt voluptate.",
+        props: {
+            meetup: {
+                id: route,
+                img: "https://picsum.photos/3840/2160",
+                title: "A First Meetup",
+                address: "STreet no 5 block 21B",
+                description: "Deserunt aute sint laborum quis nisi deserunt voluptate.",
             }
         }
     }
